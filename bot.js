ef4d305ed591efbe713451b281ab24c248e99907
youkhanna2001 <53185174+youkhanna2001@users.noreply.github.com>
2020-07-26 02:13:37 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['Patrick_star#6398'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
